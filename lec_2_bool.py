65cdf10f5c1186d61a9fb7995e1e07dff973037b
eruntomn <114501121+eruntomn@users.noreply.github.com>
2022-10-11 16:47:41 +0000

    11.10

---
+++ lec_2_bool.py
@@ -0,0 +1,15 @@
+#булевые элементы - True и False
+print(bool(2)) #Всё, что подаётся на вход bool - высказывание
+
+print(bool('Good'))
+
+print(bool([1, 4, 5]))
+
+print(bool(0))
+
+print(bool(''))
+
+print(bool([]))
+
+print(bool([[]]))
+
