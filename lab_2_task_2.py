fddb4f96a81fff6fd1b5972d8f7746ee78b87e11
eruntomn <114501121+eruntomn@users.noreply.github.com>
2022-10-16 15:58:37 +0000

    11.10

---
+++ lab_2_task_2.py
@@ -0,0 +1,4 @@
+b1 = int(input())
+q = int(input())
+n = int(input())
+print(b1, b1*q**(n-1), b1*q**(n-2), b1*q**(n-3))
---
+++ lab_2_task_3.py
@@ -0,0 +1,5 @@
+a = int(input())
+if a == 366:
+  print('високосный')
+else:
+  print('не високосный')
