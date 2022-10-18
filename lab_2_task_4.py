577972a5187d4f7a59d34f6a8cfc4b30e97ffc57
eruntomn <114501121+eruntomn@users.noreply.github.com>
2022-10-18 16:41:53 +0000

    18.10

--- lab_2_task_4.py
+++ lab_2_task_4.py
@@ -1,8 +1,10 @@
 n = int(input())
-n1 = 1
-n2 = 1
+n1 = n2 = 1
+print(n1)
+print(n2)
 for i in range(n):
-  n = n1 + n2
-  print(n)
-  n = n1
-  n1 = n2
+  nn = n1 + n2
+  print(nn)
+  nn = n2 + nn
+  nn = n2
+  nn = n2 
