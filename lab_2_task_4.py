78d6f24e2b05808b4e2c9747c1be230c385aa65f
eruntomn <114501121+eruntomn@users.noreply.github.com>
2022-10-23 11:11:54 +0000

    25.10

--- lab_2_task_4.py
+++ lab_2_task_4.py
@@ -1,10 +1,6 @@
+fib1 = fib2 = 1
 n = int(input())
-n1 = n2 = 1
-print(n1)
-print(n2)
-for i in range(n):
-  nn = n1 + n2
-  print(nn)
-  nn = n2 + nn
-  nn = n2
-  nn = n2 
+print(fib1, fib2, end=' ')
+for i in range(2, n):
+    fib1, fib2 = fib2, fib1 + fib2
+    print(fib2, end=' ')
