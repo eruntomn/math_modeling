823e1c07998baf12c608915329c2af2947236c84
eruntomn <114501121+eruntomn@users.noreply.github.com>
2022-12-27 14:26:34 +0000

    27.12

--- lab_7_task_2.py
+++ lab_7_task_2.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
-object = plt.plot([], [], 'o', color='r', label='ball')
+ball, = plt.plot([], [], 'o', color='b', label='Ball')
 
-def update(R):
-    t = np.arange(0, 2*np.pi, 0.1)
-    x = R * np.cos(t)
-    y = R * np.sin(t)
+def circle_move(R, vx0, vy0, time):
+    x0 = vx0
+    y0 = vy0
+    alpha=np.arange(0, 2*np.pi, 0.1)
+    x = x0 + R*np.cos(alpha)
+    y = y0 + R*np.sin(alpha)
     return x, y
 
 edge = 3
@@ -17,7 +19,7 @@ ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
 def animate(i):
-    ball.set_data(update(R-i))
+    ball.set_data(circle_move(R=0.01*i, vx0=0.01, vy0=0.01, time=i))
 
-ani = FuncAnimation(fig, update, frames=np.arange(0, 3, 0.1), interval=100)
+ani = animation.FuncAnimation(fig, animate, frames=100, interval=30)
 ani.save('task3.gif')
