e493e8b11b018359c52da39fd19e8f4a7edd4121
eruntomn <114501121+eruntomn@users.noreply.github.com>
2023-01-10 15:33:31 +0000

    10.01

--- project.py
+++ project.py
@@ -4,7 +4,7 @@ import matplotlib.animation as animation
 
 
 fig, ax = plt.subplots()
-ball1, = plt.plot([], [], 'o', color='b', label='Ball')
+ball1, = plt.plot([], [], 'o', color='y', label='Ball')
 
 def circle_move(R, vx0, vy0, time):
     x0 = vx0 * time
@@ -18,7 +18,7 @@ ax.set_xlim(-5, 10)
 ax.set_ylim(-100, 100)  # поменять направление
 
 def animate(i):
-    ball1.set_data(circle_move(R=0.1, vx0=0.01, vy0=0.01, time=i))
+    ball1.set_data(circle_move(R=0.1, vx0=0.01, vy0=0.01, time=2*i))
 
 ani = animation.FuncAnimation(fig, animate, frames=360, interval=30)
 ani.save('proba.gif')
